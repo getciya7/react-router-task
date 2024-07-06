4159a508744221a1b08433912fd73324264fc1d3
Getci123 <getciya7@gmail.com>
2024-07-06 13:55:39 +0530

    data

---
+++ src/data/courses.js
@@ -0,0 +1,273 @@
+// src/data/courses.js
+const courses = {
+  all: [
+    {
+      id: 1,
+      title: "10 Best React Project Ideas for Developers",
+      image:
+        "/img/10-Best-React-Project-Ideas-for-Developers-with-Source-Code.webp",
+    },
+    {
+      id: 2,
+      title: "Full Stack Developer Roadmap: A Complete Guide [2024]",
+      image: "/img/full_stack_developer_roadmap_in_2024.webp",
+    },
+    {
+      id: 3,
+      title: "Backend Development Roadmap: A Complete Guide 2024",
+      image:
+        "/img/Feature-image-Backend-Development-Roadmap-A-Complete-Guide-768x480.webp",
+    },
+    {
+      id: 4,
+      title: "Top 10 Product-Based Companies for Full-Stack Developers [2024]",
+      image:
+        "/img/Featured-Image-top-product-based-companies-for-full-stack-developers-768x480.webp",
+    },
+    {
+      id: 5,
+      title: "The Future & Scope of Full-Stack Developers in India",
+      image: "/img/24746653_7002417-1-1536x1024.jpg",
+    },
+    {
+      id: 6,
+      title: "7 Best Full-Stack Development Online Courses [2024]",
+      image:
+        "/img/Feature-image-Best-Full-Stack-Development-Online-Courses-768x480.webp",
+    },
+    {
+      id: 7,
+      title:
+        "Comprehensive List of Project Ideas for Frontend Development [2024]",
+      image: "/img/Project-Ideas-for-Frontend-Development.webp",
+    },
+    {
+      id: 8,
+      title: "A Comprehensive Guide to HTML and CSS Roadmap [2024]",
+      image: "/img/html_and_css_roadmap.webp",
+    },
+    {
+      id: 9,
+      title: "How to Create a Simple “Hello World” Page Using HTML?",
+      image:
+        "/img/Feature-image-Steps-to-Create-a-Simple-Hello-World-Page-Using-HTML-768x480.webp",
+    },
+    {
+      id: 10,
+      title: "How Long Would It Take to Learn Data Science?",
+      image: "/img/How-Long-Would-It-Take-to-Learn-Data-Science-1536x804.webp",
+    },
+    {
+      id: 11,
+      title: "Everything about Data Scientist Salary in India | 2024",
+      image:
+        "/img/Everything-about-Data-Scientist-Salary-in-India-2023-1536x804.webp",
+    },
+    {
+      id: 12,
+      title: "How to become a Data Scientist after Mechanical Engineering?",
+      image: "/img/paper-analysis-2-1536x1024.jpg",
+    },
+    {
+      id: 13,
+      title: "Data Science vs Data Analytics | Best Career Choice in 2024",
+      image: "/img/Your-paragraph-text-1536x768.webp",
+    },
+    {
+      id: 14,
+      title: "Top Product-Based Companies for Data Scientists in 2024",
+      image:
+        "/img/Feature-image-Top-Product-Based-Companies-for-Data-Scientists-768x480.webp",
+    },
+    {
+      id: 15,
+      title: "AI vs ML vs Data Science: What Should You Learn In 2024?",
+      image: "/img/2.webp",
+    },
+    {
+      id: 16,
+      title: "The Ultimate Cybersecurity Roadmap for Beginners",
+      image:
+        "/img/The-Ultimate-%E2%80%A8Cybersecurity-Roadmap-for-Beginners.webp",
+    },
+    {
+      id: 17,
+      title:
+        "The Cybersecurity Surge: 5 Must-Have Cybersecurity Certifications!",
+      image: "/img/Blog-9-600x314.webp",
+    },
+    {
+      id: 18,
+      title: "How Is Cyber Security Important To Our Lives?",
+      image: "/img/cyber-security-e1614342903477.webp",
+    },
+    {
+      id: 19,
+      title:
+        "What is Cybersecurity? Importance and its uses & the growing challenge...",
+      image: "/img/cybersecuity-.gif",
+    },
+    {
+      id: 20,
+      title:
+        "Crack the TCS IPA Exam: Top Questions and Exclusive Tips for Success!",
+      image: "/img/image-18.webp",
+    },
+    {
+      id: 21,
+      title:
+        "30 Best JavaScript Project Ideas For You [3 Bonus Portfolio Projects]",
+      image: "/img/animated-javascript-project-image-1200x675.jpg",
+    },
+    {
+      id: 22,
+      title: "Top 9 TCS Xplore Python Coding Questions",
+      image: "/img/6.webp",
+    },
+    {
+      id: 23,
+      title:
+        "10 Innovative Project Ideas for Students to Boost Creativity and Skills",
+      image: "/img/featured-image.webp",
+    },
+  ],
+  fullstack: [
+    {
+      id: 1,
+      title: "10 Best React Project Ideas for Developers",
+      image:
+        "/img/10-Best-React-Project-Ideas-for-Developers-with-Source-Code.webp",
+    },
+    {
+      id: 2,
+      title: "Full Stack Developer Roadmap: A Complete Guide [2024]",
+      image: "/img/full_stack_developer_roadmap_in_2024.webp",
+    },
+    {
+      id: 3,
+      title: "Backend Development Roadmap: A Complete Guide 2024",
+      image:
+        "/img/Feature-image-Backend-Development-Roadmap-A-Complete-Guide-768x480.webp",
+    },
+    {
+      id: 4,
+      title: "Top 10 Product-Based Companies for Full-Stack Developers [2024]",
+      image:
+        "/img/Featured-Image-top-product-based-companies-for-full-stack-developers-768x480.webp",
+    },
+    {
+      id: 5,
+      title: "The Future & Scope of Full-Stack Developers in India",
+      image: "/img/24746653_7002417-1-1536x1024.jpg",
+    },
+    {
+      id: 6,
+      title: "7 Best Full-Stack Development Online Courses [2024]",
+      image:
+        "/img/Feature-image-Best-Full-Stack-Development-Online-Courses-768x480.webp",
+    },
+    {
+      id: 7,
+      title:
+        "Comprehensive List of Project Ideas for Frontend Development [2024]",
+      image: "/img/Project-Ideas-for-Frontend-Development.webp",
+    },
+    {
+      id: 8,
+      title: "A Comprehensive Guide to HTML and CSS Roadmap [2024]",
+      image: "/img/html_and_css_roadmap.webp",
+    },
+    {
+      id: 9,
+      title: "How to Create a Simple “Hello World” Page Using HTML?",
+      image:
+        "/img/Feature-image-Steps-to-Create-a-Simple-Hello-World-Page-Using-HTML-768x480.webp",
+    },
+  ],
+  datascience: [
+    {
+      id: 10,
+      title: "How Long Would It Take to Learn Data Science?",
+      image: "/img/How-Long-Would-It-Take-to-Learn-Data-Science-1536x804.webp",
+    },
+    {
+      id: 11,
+      title: "Everything about Data Scientist Salary in India | 2024",
+      image:
+        "/img/Everything-about-Data-Scientist-Salary-in-India-2023-1536x804.webp",
+    },
+    {
+      id: 12,
+      title: "How to become a Data Scientist after Mechanical Engineering?",
+      image: "/img/paper-analysis-2-1536x1024.jpg",
+    },
+    {
+      id: 13,
+      title: "Data Science vs Data Analytics | Best Career Choice in 2024",
+      image: "/img/Your-paragraph-text-1536x768.webp",
+    },
+    {
+      id: 14,
+      title: "Top Product-Based Companies for Data Scientists in 2024",
+      image:
+        "/img/Feature-image-Top-Product-Based-Companies-for-Data-Scientists-768x480.webp",
+    },
+    {
+      id: 15,
+      title: "AI vs ML vs Data Science: What Should You Learn In 2024?",
+      image: "/img/2.webp",
+    },
+  ],
+  cybersecurity: [
+    {
+      id: 16,
+      title: "The Ultimate Cybersecurity Roadmap for Beginners",
+      image:
+        "/img/The-Ultimate-%E2%80%A8Cybersecurity-Roadmap-for-Beginners.webp",
+    },
+    {
+      id: 17,
+      title:
+        "The Cybersecurity Surge: 5 Must-Have Cybersecurity Certifications!",
+      image: "/img/Blog-9-600x314.webp",
+    },
+    {
+      id: 18,
+      title: "How Is Cyber Security Important To Our Lives?",
+      image: "/img/cyber-security-e1614342903477.webp",
+    },
+    {
+      id: 19,
+      title:
+        "What is Cybersecurity? Importance and its uses & the growing challenge...",
+      image: "/img/cybersecuity-.gif",
+    },
+  ],
+  career: [
+    {
+      id: 20,
+      title:
+        "Crack the TCS IPA Exam: Top Questions and Exclusive Tips for Success!",
+      image: "/img/image-18.webp",
+    },
+    {
+      id: 21,
+      title:
+        "30 Best JavaScript Project Ideas For You [3 Bonus Portfolio Projects]",
+      image: "/img/animated-javascript-project-image-1200x675.jpg",
+    },
+    {
+      id: 22,
+      title: "Top 9 TCS Xplore Python Coding Questions",
+      image: "/img/6.webp",
+    },
+    {
+      id: 23,
+      title:
+        "10 Innovative Project Ideas for Students to Boost Creativity and Skills",
+      image: "/img/featured-image.webp",
+    },
+  ],
+};
+
+export default courses;
